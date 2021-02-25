baaa2e61ed18186ffc02885ca0db0b531a32d80d
dubiZA <darius@dubiza.dev>
2021-02-25 08:59:47 -0700

    Add click command grouping and enhance functionality
    Adding click command grouping foundations to allow for more
    natural execution of desired outcome. For example, if a URL
    needs to be decoded: corax decode url <url>.
    Added function for the following commands:
    - URL decoding
    - Base64 decoding

--- corax.py
+++ corax.py
@@ -1,30 +1,55 @@
 import click
 import requests
+import urllib.parse
+import base64
 
 
-def decoder_proofpoint(url):
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.argument('url')
+def sanitize(url):
+    no_scheme = url.replace('http', 'hxxp')
+    no_domains = no_scheme.replace('.', '[.]')
+    click.echo('Sanitized URL: ', nl=False)
+    click.secho(no_domains, fg='green')
+
+
+@click.group()
+def decode():
+    pass
+
+
+cli.add_command(decode)
+
+@decode.command('proofpoint')
+@click.argument('url')
+def decode_proofpoint(url):
     encoded_url = {
         'urls': [url]
     }
-
-    response = requests.post('https://tap-api-v2.proofpoint.com/v2/url/decode',
-                                json=encoded_url)
-    
-    return response.json()['urls'][0]['decodedUrl']
-
-
-def sanitize_url(url):
-    no_scheme = url.replace('http', 'hxxp')
-    no_domains = no_scheme.replace('.', '[.]')
-
-    return no_domains
+    response = requests.post(
+        'https://tap-api-v2.proofpoint.com/v2/url/decode',
+        json=encoded_url
+        )
+    click.echo('Proofpointed Decoded URL: ', nl=False)
+    click.secho(response.json()['urls'][0]['decodedUrl'], fg='green')
 
 
-@click.command()
-@click.argument('input')
-def cli(input):
-    if 'https://urldefense.' in input:
-        decoder_proofpoint(input)
-        click.secho(f'\n{decoder_proofpoint(input)}', fg='green')
-    elif 'https://' in input or 'http://' in input:
-        click.secho(f'\n{sanitize_url(input)}', fg='green')
+@decode.command('url')
+@click.argument('url')
+def decode_url(url):
+    decoded_url = urllib.parse.unquote_plus(url)
+    click.echo('Decoded URL: ', nl=False)
+    click.secho(decoded_url, fg='green')
+
+
+@decode.command('base64')
+@click.argument('base64_string')
+def decode_base64(base64_string):
+    decoded_base64 = str(base64.b64decode(base64_string), 'utf-8')
+    click.echo('Decoded String: ', nl=False)
+    click.secho(decoded_base64, fg='green')
